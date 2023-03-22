f02f47b59a5b7205593e41f12f08b0cbeee5ff37
ArmandPAquino <ArmandPAquino@gmail.com>
2023-03-22 16:23:19 -0700

    update method return values based on requirements

--- 591_HW8_Battleship/src/battleship/EmptySea.java
+++ 591_HW8_Battleship/src/battleship/EmptySea.java
@@ -1,32 +1,34 @@
 package battleship;
 
 public class EmptySea extends Ship {
-	
+
 	// static variables
 
 	static final int LENGTH = 1;
 	static final String TYPE = "empty";
 
-
+	// constructor
 	public EmptySea() {
 		super(EmptySea.LENGTH);
 	}
 
 	@Override
 	boolean shootAt(int row, int column) {
+		// always returns false
 		return false;
 	}
-	
+
 	@Override
 	boolean isSunk() {
+		// always returns false
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "";
+		return "-";
 	}
-	
+
 	@Override
 	public String getShipType() {
 		return EmptySea.TYPE;
